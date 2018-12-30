904393f75b2cd298fc0eef2085c65e287b8320e6
wangcanfeng01 <373811598@qq.com>
2018-12-30 15:18:40 +0800

    delete
    删除不必要的文件

--- front-ui/target/classes/static/admin/footer-content.js
+++
@@ -1,6 +0,0 @@
-document.writeln(" <footer>");
-document.writeln("            <div class=\'pull-right\'>");
-document.writeln("                网站开发设计 <a href=\'https://www.canfeng.xyz\'>Canfeng Wang</a>");
-document.writeln("            </div>");
-document.writeln("            <div class=\'clearfix\'></div>");
-document.writeln("        </footer>");
--- front-ui/target/classes/static/common/sweetalert2/src/globalState.js
+++
@@ -1,5 +0,0 @@
-import defaultParams from './utils/params.js'
-
-export default {
-  popupParams: Object.assign({}, defaultParams)
-}
