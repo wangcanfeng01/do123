cb05265580e4d8b375b52839407fd06d0352d63e
wangcanfeng01 <373811598@qq.com>
2019-01-13 23:06:33 +0800

    delete
    删除不必要的前端文件

--- front-ui/src/main/resources/static/common/meta-icon.js
+++
@@ -1,2 +0,0 @@
-document.writeln("    <meta name=\'viewport\' content=\'width=device-width, initial-scale=1\'/>");
-document.writeln("    <link rel=\'shortcut icon\' href=\'../static/images/banner.gif\'/>");
