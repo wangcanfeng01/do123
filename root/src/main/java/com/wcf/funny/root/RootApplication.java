7ae826ffcab85898fa61c7e84333416d6f8219ea
wangcanfeng01 <32534120+wangcanfeng01@users.noreply.github.com>
2019-02-28 10:32:54 +0800

    Update RootApplication.java

--- root/src/main/java/com/wcf/funny/root/RootApplication.java
+++ root/src/main/java/com/wcf/funny/root/RootApplication.java
@@ -3,13 +3,20 @@ package com.wcf.funny.root;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = "com.wcf.funny")
 @MapperScan("com.wcf.funny.**.mapper")
-public class RootApplication {
+public class RootApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(RootApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(RootApplication.class);
 	}
 
 }
