8fa48234f05657539ffddf1b9e29f0a0c3f97196
wangcanfeng01 <32534120+wangcanfeng01@users.noreply.github.com>
2019-03-05 11:41:02 +0800

    Update MenuErrorCode.java

--- admin/src/main/java/com/wcf/funny/admin/exception/errorcode/MenuErrorCode.java
+++ admin/src/main/java/com/wcf/funny/admin/exception/errorcode/MenuErrorCode.java
@@ -9,15 +9,15 @@ import com.wcf.funny.core.exception.errorcode.CoreCode;
  **/
 public enum MenuErrorCode implements CoreCode {
     //插入菜单异常
-    INSERT_MENU_ERROR("00000005", "Insert menu info into database failed"),
+    INSERT_MENU_ERROR("0x02010001", "Insert menu info into database failed"),
     //搜索菜单异常
-    SELECT_MENU_ERROR("00000006", "Select menu info from database failed"),
+    SELECT_MENU_ERROR("0x02010002", "Select menu info from database failed"),
     //更新菜单异常
-    UPDATE_MENU_ERROR("00000007", "Update menu info into database failed"),
+    UPDATE_MENU_ERROR("0x02010003", "Update menu info into database failed"),
     //删除菜单异常
-    DELETE_MENU_ERROR("00000008", "Delete menu info from database failed"),
+    DELETE_MENU_ERROR("0x02010004", "Delete menu info from database failed"),
     //菜单信息已存在
-    MENU_ALREADY_EXIST("00000008", "this menu is already exist in database"),;
+    MENU_ALREADY_EXIST("0x02010005", "this menu is already exist in database"),;
 
     /**
      * 错误信息
