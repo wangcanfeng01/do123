18c3b1094eef117cc3107a80b0971fa626a966e7
wangcanfeng01 <32534120+wangcanfeng01@users.noreply.github.com>
2019-03-05 11:42:04 +0800

    Update UserErrorCode.java

--- admin/src/main/java/com/wcf/funny/admin/exception/errorcode/UserErrorCode.java
+++ admin/src/main/java/com/wcf/funny/admin/exception/errorcode/UserErrorCode.java
@@ -8,26 +8,26 @@ import com.wcf.funny.core.exception.errorcode.CoreCode;
  * @function
  **/
 public enum UserErrorCode implements CoreCode {
-    //插入用户异常
-    INSERT_USER_ERROR("00000005", "Insert user data into database failed"),
+     //插入用户异常
+    INSERT_USER_ERROR("0x02030001", "Insert user data into database failed"),
     //搜索用户异常
-    SELECT_USER_ERROR("00000006", "Select user data from database failed"),
+    SELECT_USER_ERROR("0x02030002", "Select user data from database failed"),
     //更新用户异常
-    UPDATE_USER_ERROR("00000007", "Update user data into database failed"),
+    UPDATE_USER_ERROR("0x02030003", "Update user data into database failed"),
     //删除用户异常
-    DELETE_USER_ERROR("00000008", "Delete user data from database failed"),
+    DELETE_USER_ERROR("0x02030004", "Delete user data from database failed"),
     //用户名已存在
-    USER_ALREADY_EXIST("00010001", "The user name is already exist"),
+    USER_ALREADY_EXIST("0x02030005", "The user name is already exist"),
     //角色查询失败
-    SELECT_ROLR_ERROR("00010001","select role from db failed"),
+    SELECT_ROLR_ERROR("0x02030006","select role from db failed"),
     //角色插入失败
-    INSETR_ROLR_ERROR("00010001","insert role from db failed"),
+    INSETR_ROLR_ERROR("0x02030007","insert role from db failed"),
     //角色更新失败
-    UPDATE_ROLR_ERROR("00010001","update role from db failed"),
+    UPDATE_ROLR_ERROR("0x02030008","update role from db failed"),
     //角色删除失败
-    DELETE_ROLR_ERROR("00010001","delete role from db failed"),
+    DELETE_ROLR_ERROR("0x02030009","delete role from db failed"),
     //登录用户信息存在异常
-    LOGIN_USER_INFO_ERROR("000010001","the login user has something not right"),
+    LOGIN_USER_INFO_ERROR("0x0203000A","the login user has something not right"),
     ;
 
     /**
