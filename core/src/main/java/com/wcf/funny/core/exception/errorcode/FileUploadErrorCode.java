d720af94f091d4dbb75c3448e88652b7299ebbdb
wangcanfeng01 <32534120+wangcanfeng01@users.noreply.github.com>
2019-03-05 14:03:11 +0800

    Update FileUploadErrorCode.java

--- core/src/main/java/com/wcf/funny/core/exception/errorcode/FileUploadErrorCode.java
+++ core/src/main/java/com/wcf/funny/core/exception/errorcode/FileUploadErrorCode.java
@@ -6,14 +6,16 @@ package com.wcf.funny.core.exception.errorcode;
  * @function 文件上传错误码
  **/
 public enum  FileUploadErrorCode implements CoreCode{
-    // 插入图片记录信息异常
-    FILE_INFO_INSERT_ERROR("00000014", "insert the file information to db failed"),
+     // 插入图片记录信息异常
+    FILE_INFO_INSERT_ERROR("0x05030001", "insert the file information to db failed"),
     // 删除图片记录信息异常
-    FILE_INFO_DELETE_ERROR("00000014", "delete the file information to db failed"),
+    FILE_INFO_DELETE_ERROR("0x05030002", "delete the file information to db failed"),
     // 查询图片记录信息异常
-    FILE_INFO_SELECT_ERROR("00000014", "select the file information to db failed"),
-    FILE_COPY_TO_DIRECTORY_ERROR("0000100001","copy file to directory failed"),
-    FILE_PATH_NULL("00010001","the file path can not be null"),
+    FILE_INFO_SELECT_ERROR("0x05030003", "select the file information to db failed"),
+    // 图片复制到指定文件夹异常
+    FILE_COPY_TO_DIRECTORY_ERROR("0x05030004","copy file to directory failed"),
+    // 文件路径不能为空
+    FILE_PATH_NULL("0x05030005","the file path can not be null"),
     ;
     /**
      * 原因
