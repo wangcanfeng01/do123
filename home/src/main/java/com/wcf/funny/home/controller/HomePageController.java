a31c83171904f8055129b19bd88367a0b2f6d4e6
wangcanfeng01 <373811598@qq.com>
2019-03-10 22:56:02 +0800

    修改主页相关url跳转

--- home/src/main/java/com/wcf/funny/home/controller/HomePageController.java
+++ home/src/main/java/com/wcf/funny/home/controller/HomePageController.java
@@ -20,36 +20,98 @@ public class HomePageController {
      */
     @RequestMapping({"/home", "/"})
     public void home(HttpServletResponse response) throws IOException {
-       response.sendRedirect("/index.html");
+        response.sendRedirect("/index.html");
     }
 
+    /**
+     * 功能描述：访问后台页面时跳转到前端登录路由
+     *
+     * @param
+     * @author wangcanfeng
+     * @time 2019/3/10 12:05
+     * @since v1.0
+     **/
     @RequestMapping("/login")
-    public String login(){
+    public String login() {
         return "/index.html";
     }
 
+    /**
+     * 功能描述：访问后台页面时跳转到前端登出路由
+     *
+     * @param
+     * @author wangcanfeng
+     * @time 2019/3/10 12:05
+     * @since v1.0
+     **/
     @RequestMapping("/logout")
-    public String logout(){
+    public String logout() {
         return "/index.html";
     }
 
+    /**
+     * 功能描述：访问后台页面时跳转到前端管理界面
+     *
+     * @param
+     * @author wangcanfeng
+     * @time 2019/3/10 12:05
+     * @since v1.0
+     **/
     @RequestMapping("/admin/**")
-    public String admin(){
+    public String admin() {
         return "/index.html";
     }
 
+    /**
+     * 功能描述：访问后台页面时跳转到前端个人信息页面
+     *
+     * @param
+     * @author wangcanfeng
+     * @time 2019/3/10 12:05
+     * @since v1.0
+     **/
+    @RequestMapping("/personInfo/**")
+    public String personalInfo() {
+        return "/index.html";
+    }
+
+    /**
+     * 功能描述：访问后台页面时跳转到前端博客页面
+     *
+     * @param
+     * @author wangcanfeng
+     * @time 2019/3/10 12:05
+     * @since v1.0
+     **/
     @RequestMapping("/blog/**")
-    public String blog(){
+    public String blog() {
         return "/index.html";
     }
 
+
+    /**
+     * 功能描述：访问后台页面时跳转到前端视频播放页面
+     *
+     * @param
+     * @author wangcanfeng
+     * @time 2019/3/10 12:05
+     * @since v1.0
+     **/
     @RequestMapping("/video/**")
-    public String video(){
+    public String video() {
         return "/index.html";
     }
 
+    /**
+     * 功能描述：访问后台页面时跳转到前端版本展示
+     *
+     * @param
+     * @author wangcanfeng
+     * @time 2019/3/10 12:05
+     * @since v1.0
+     **/
     @RequestMapping("/about/**")
-    public String about(){
+    public String about() {
         return "/index.html";
     }
 
